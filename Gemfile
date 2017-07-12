9c2925b6d6e9cd1a6785499fedd90469c4407d16
Arun Madisetty <arun.madisetty@gmail.com>
2017-07-11 20:29:03 -0500

    Made website more RESTful

--- Gemfile
+++ Gemfile
@@ -12,6 +12,8 @@ gem 'rails', '~> 5.1.2'
 gem 'pg', '~> 0.18'
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
+gem 'bootstrap-sass', '~> 3.3.6'
+gem 'jquery-rails', '~> 4.3.1' 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,9 +1,55 @@
 class ContactsController < ApplicationController
-  def contacts_method
+  
+  def index
     @contacts = Contact.all
-    @first = Contact.find_by(id: 1)
-    @second = Contact.find_by(id: 2)
-    @third = Contact.find_by(id: 3)
-    render "contacts.html.erb"
+    render "index.html.erb"
+  end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    newcontact = Contact.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+      )
+    newcontact.save
+    #render "create.html.erb"
+    redirect_to "/contacts"
+  end
+
+  def show
+    contact_id = params[:id]
+    @contact = Contact.find_by(id: contact_id)
+    render "show.html.erb"
+  end
+
+  def edit
+    contact_id = params[:id]
+    @contact = Contact.find_by(id: contact_id)
+    render "edit.html.erb"
+  end
+
+  def update
+    contact_id = params[:id]
+    @contact = Contact.find_by(id: contact_id)
+    @contact.first_name = params[:first_name]
+    @contact.last_name = params[:last_name]
+    @contact.email = params[:email]
+    @contact.phone_number = params[:phone_number]
+    @contact.save
+    #render "update.html.erb"
+    redirect_to "/contacts/#{@contact.id}"
+  end
+
+  def destroy
+    contact_id = params[:id]
+    @contact = Contact.find_by(id: contact_id)
+    @contact.destroy
+    #render "destroy.html.erb"
+    redirect_to "/contacts"
   end
 end
